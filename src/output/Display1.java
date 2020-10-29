df13b8dd15d43dec2d0a520eb1d23b0c3b97265b
grahamRuttiman <graham.ruttiman@my.jcu.edu.au>
2020-10-29 14:59:48 +1000

    work on Display1

--- src/output/Display1.java
+++ src/output/Display1.java
@@ -1,8 +1,24 @@
 package output;
 
+import javax.swing.*;
+
 public class Display1 {
 
+    public String displayMessage;
+
     public static void setDisplay1(String message){
+        displayMessage = message;
+        // Display 1
+        JFrame jFrame=new JFrame("Display 1");
+        final JTextField Display1 =new JTextField();
+        Display1.setBounds(25,25, 335,50);
+        Display1.setText(message);
 
     }
+
+    public static String getDisplay1(){
+
+    }
+
+
 }
