033694df27de56447e946e722c83b8b4ee530982
grahamRuttiman <graham.ruttiman@my.jcu.edu.au>
2020-10-30 17:40:54 +1000

    added database query example to Controller() class.

--- src/insulinPumpController/Controller.java
+++ src/insulinPumpController/Controller.java
@@ -15,6 +15,7 @@ import static insulinPumpController.StatusValue.WARNING;
 import static insulinPumpController.SwitchValue.OFF;
 import static output.Alarm.setAlarm;
 import static output.Display1.setDisplay1;
+import java.sql.*;
 
 public class Controller {
 
@@ -54,6 +55,41 @@ public class Controller {
         // every time a clock timer takes reading.
         Demo demo = new Demo();
         r2 = demo.getReading();
+
+        //Database from SQL example using eBookshop database
+        try (
+                // Step 1: Allocate a database 'Connection' object
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                        "myuser", "xxxx");   // For MySQL only
+                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+
+                // Step 2: Allocate a 'Statement' object in the Connection
+                Statement stmt = conn.createStatement();
+        ) {
+            // Step 3: Execute a SQL SELECT query. The query result is returned in a 'ResultSet' object.
+            String strSelect = "select title, price, qty from books";
+            System.out.println("The SQL statement is: " + strSelect + "\n"); // Echo For debugging
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+            //  For each row, retrieve the contents of the cells with getXxx(columnName).
+            System.out.println("The records selected are:");
+            int rowCount = 0;
+            while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+                String title = rset.getString("title");
+                double price = rset.getDouble("price");
+                int    qty   = rset.getInt("qty");
+                System.out.println(title + ", " + price + ", " + qty);
+                ++rowCount;
+            }
+            System.out.println("Total number of records = " + rowCount);
+
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
+
 
         // STARTUP
         if (switchValue == OFF || switchValue == SwitchValue.AUTO) {
